84c41866845f8accc12031be4247cd3297a2ece1
psn-soliveri <psn-soliveri@users.noreply.github.com>
2022-03-14 09:54:08 +0000

    deploy: 6d753b2e25ca006146688cadd3086545dfa91176

---
+++ precache-manifest.513e114fad2b552925cc4fcc7eb34de9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8742256a68added5bf17d5adc8d48f7b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2ca8950c84807fa58f09",
+    "url": "/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "b8b764281ba1020ab675",
+    "url": "/static/js/2.db37001b.chunk.js"
+  },
+  {
+    "revision": "2ca8950c84807fa58f09",
+    "url": "/static/js/main.998ec843.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
